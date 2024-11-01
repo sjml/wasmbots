e53bed94e284a946044996df426ce62afa59bc7b
Shane Liesegang <shane@techie.net>
2024-11-01 12:14:11 +0800

    need to change bot loader if I want to repath this

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { goto } from "$app/navigation";
-    goto("./app", { replaceState: true });
+    import App from "../ui/App.svelte";
 </script>
 
+<App />
--- frontend/src/routes/app/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-    import App from "../../ui/App.svelte";
-</script>
-
-<App />
