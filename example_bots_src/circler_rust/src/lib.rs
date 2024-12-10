d2b8db3e71a3a6425ae3006b7b65ca7ae9199c81
Shane Liesegang <shane@techie.net>
2024-12-10 16:41:07 +0800

    circler refinement

--- example_bots_src/circler_rust/src/lib.rs
+++ example_bots_src/circler_rust/src/lib.rs
@@ -47,30 +47,31 @@ fn client_setup() -> params::BotMetadata {
 }
 
 fn tick(pc: wasmbot_messages::PresentCircumstances) -> wasmbot_messages::Message {
-	let last_move_was_door = LAST_MOVE_TYPE.with_borrow(|dir| match *dir {
+	let last_move_was_door = LAST_MOVE_TYPE.with_borrow(|lmt| match *lmt {
 		wasmbot_messages::MessageType::Open => true,
 		wasmbot_messages::MessageType::Close => true,
 		_ => false
 	});
 
+	let side_len = pc.surroundings_radius * 2 + 1;
+	let grid = Grid::new(pc.surroundings, side_len.into(), side_len.into());
+	let origin = Point { x: pc.surroundings_radius.into(), y: pc.surroundings_radius.into() };
+
 	if !last_move_was_door {
-		let side_len = pc.surroundings_radius * 2 + 1;
-		let grid = Grid::new(pc.surroundings, side_len.into(), side_len.into());
 		let ns = Point::get_neighbors_4(Point{x: 0, y: 0});
-		let origin = Point { x: pc.surroundings_radius.into(), y: pc.surroundings_radius.into() };
 		for pt in &ns {
 			let ntile = grid.get_from_origin(&origin, pt);
 			match ntile {
 				wasmbot_messages::TileType::ClosedDoor => {
 					log(&std::format!("opening door @ ({}, {})", pt.x, pt.y));
-					LAST_MOVE_TYPE.with_borrow_mut(|dir| *dir = wasmbot_messages::MessageType::Open);
+					LAST_MOVE_TYPE.with_borrow_mut(|lmt| *lmt = wasmbot_messages::MessageType::Open);
 					return wasmbot_messages::Message::Open(wasmbot_messages::Open {
 						target: pt.to_msg_pt()
 					});
 				},
 				wasmbot_messages::TileType::OpenDoor => {
 					log(&std::format!("closing door @ ({}, {})", pt.x, pt.y));
-					LAST_MOVE_TYPE.with_borrow_mut(|dir| *dir = wasmbot_messages::MessageType::Close);
+					LAST_MOVE_TYPE.with_borrow_mut(|lmt| *lmt = wasmbot_messages::MessageType::Close);
 					return wasmbot_messages::Message::Close(wasmbot_messages::Close {
 						target: pt.to_msg_pt()
 					});
@@ -80,16 +81,24 @@ fn tick(pc: wasmbot_messages::PresentCircumstances) -> wasmbot_messages::Message
 		}
 	}
 
-	if last_move_was_door || pc.last_move_result == wasmbot_messages::MoveResult::Failed {
-		let mut diri = DIRECTION.with_borrow(|dir| *dir as u8);
-		diri += 2;
-		diri %= 8;
-		log(&std::format!("new direction: {}", diri));
-		DIRECTION.with_borrow_mut(|dir| *dir = (diri).try_into().expect("Bad direction"));
+	let mut dir = DIRECTION.with_borrow(|dir| *dir);
+	let delta = exploration::MOVEMENT[dir as usize];
+	let peek = grid.get_from_origin(&origin, &Point{x: delta.x.into(), y: delta.y.into()});
+
+	match peek {
+		wasmbot_messages::TileType::Floor => {},
+		_ => {
+			log("changing direction because next tile is not floor");
+			let mut diri = DIRECTION.with_borrow(|dir| *dir as u8);
+			diri += 2;
+			diri %= 8;
+			log(&std::format!("new direction: {}", diri));
+			dir = diri.try_into().unwrap();
+			DIRECTION.with_borrow_mut(|dir| *dir = (diri).try_into().expect("Bad direction"));
+		}
 	}
 
-	let dir = DIRECTION.with(|dir| *dir.borrow());
-	LAST_MOVE_TYPE.with_borrow_mut(|dir| *dir = wasmbot_messages::MessageType::MoveTo);
+	LAST_MOVE_TYPE.with_borrow_mut(|lmt| *lmt = wasmbot_messages::MessageType::MoveTo);
 	return wasmbot_messages::Message::MoveTo(wasmbot_messages::MoveTo { direction: dir, distance: 1 })
 }
 
