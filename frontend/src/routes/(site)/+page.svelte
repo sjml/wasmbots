5f307e03b44da48b1000dfa0e3afe1af1616f78d
Shane Liesegang <shane@techie.net>
2025-01-14 14:33:34 +0800

    vidja

--- frontend/src/routes/(site)/+page.svelte
+++ frontend/src/routes/(site)/+page.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-	import { onMount } from "svelte";
-	import { goto } from "$app/navigation";
-	import { dev } from "$app/environment";
-
-	onMount(() => {
-		if (!dev) {
-			goto("./app");
-		}
-	});
+	import { base } from "$app/paths";
 </script>
 
 <svelte:head>
@@ -15,12 +7,18 @@
 </svelte:head>
 
 <div class="intro">
-	<p><em>intro video goes here</em></p>
+	<!-- svelte-ignore a11y_media_has_caption -->
+	<video src="{base}/img/docs/WasmBots_Proof-of-Concept.mp4" controls poster="{base}/img/docs/intro-video-poster.jpg"></video>
 	<p>Hi! <a href="https://shaneliesegang.com" target="_blank">I’m Shane</a>, and this is WasmBots.</p>
+	<p><em>(More to be written here…)</em></p>
 </div>
 
 <style>
 	.intro {
 		margin-top: 50px;
+	}
+
+	.intro video {
+		max-width: 100%;
 	}
 </style>
